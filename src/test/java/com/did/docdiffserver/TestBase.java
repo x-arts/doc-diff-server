b1ff25f4c59fc21646479bc41184ad1063b7a4bf
xuewenke <379435450@qq.com>
2025-04-27 09:42:23 +0800

    添加单元测试

---
+++ src/test/java/com/did/docdiffserver/TestBase.java
@@ -0,0 +1,11 @@
+package com.did.docdiffserver;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = DocDiffServerApplication.class, value = {"spring.main.allow-bean-definition-overriding=true"})
+public class TestBase {
+
+}
---
+++ src/test/java/com/did/docdiffserver/service/MdDiffServiceTest.java
@@ -0,0 +1,18 @@
+package com.did.docdiffserver.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class MdDiffServiceTest {
+
+
+
+
+    @Test
+    public void  simpleTest() {
+
+        log.info("test");
+
+    }
+}
