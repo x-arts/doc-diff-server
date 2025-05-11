66bdf5e7258ba505b6cd98154eefb1e618b6a349
xuewenke <379435450@qq.com>
2025-05-11 16:19:47 +0800

    doc to  html

---
+++ src/main/java/com/did/docdiffserver/service/PanDocService.java
@@ -0,0 +1,19 @@
+package com.did.docdiffserver.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ */
+@Slf4j
+@Service
+public class PanDocService {
+
+    public String htmlToPlanText(String html) {
+
+
+        return "";
+    }
+
+}
