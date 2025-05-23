6f8d915d6b88327e74c4c4eae272ec618d374b66
xuewenke <379435450@qq.com>
2025-05-23 20:46:44 +0800

    表格对比-debug

--- src/main/java/com/did/docdiffserver/DocDiffServerApplication.java
+++ src/main/java/com/did/docdiffserver/DocDiffServerApplication.java
@@ -1,11 +1,13 @@
 package com.did.docdiffserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.did.docdiffserver.**"})
+@MapperScan("com.did.docdiffserver.repository.mapper")
 public class DocDiffServerApplication {
 
     public static void main(String[] args) {
