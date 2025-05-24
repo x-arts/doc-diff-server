58f0f8e2a709b853e5dce14e214abac358809316
xuewenke <379435450@qq.com>
2025-05-24 18:38:15 +0800

    新加接口

--- src/main/java/com/did/docdiffserver/compent/exception/GlobalExceptionHandler.java
+++ src/main/java/com/did/docdiffserver/compent/exception/GlobalExceptionHandler.java
@@ -2,13 +2,9 @@ package com.did.docdiffserver.compent.exception;
 
 import com.did.docdiffserver.data.base.ResponseContent;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * 全局异常处理器
